f54206cbbecc55825e160bd30797f8251ea4daf7
Joseph Salgado <master@Jays-MBP-2.quinnipiac.edu>
2024-04-22 17:28:19 -0400

    update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -71,50 +71,50 @@ public class PlayerMovement : NetworkBehaviour
         }
     }
 
-    void OnCollisionEnter(Collision col)
-    {
-        if (IsClient && IsOwner)
-        {
-            // Correctly accessing the PlayerMovement component from the collision object
-            PlayerMovement otherPlayer = col.GetComponent<Collider>().GetComponent<PlayerMovement>();
-            if (otherPlayer != null && this.PlayerRole == SpawnManager.PlayerRole.Enemy && otherPlayer.PlayerRole == SpawnManager.PlayerRole.Normal)
-            {
-                Debug.Log($"Player {OwnerClientId} with role {PlayerRole} collided with player {otherPlayer.OwnerClientId} with role {otherPlayer.PlayerRole}");
-                RequestVerifyCollisionServerRpc(otherPlayer.GetComponent<NetworkObject>().NetworkObjectId);
-            }
-        }
-    }
+    // void OnCollisionEnter(Collision col)
+    // {
+    //     if (IsClient && IsOwner)
+    //     {
+    //         // Correctly accessing the PlayerMovement component from the collision object
+    //         PlayerMovement otherPlayer = col.GetComponent<Collider>().GetComponent<PlayerMovement>();
+    //         if (otherPlayer != null && this.PlayerRole == SpawnManager.PlayerRole.Enemy && otherPlayer.PlayerRole == SpawnManager.PlayerRole.Normal)
+    //         {
+    //             Debug.Log($"Player {OwnerClientId} with role {PlayerRole} collided with player {otherPlayer.OwnerClientId} with role {otherPlayer.PlayerRole}");
+    //             RequestVerifyCollisionServerRpc(otherPlayer.GetComponent<NetworkObject>().NetworkObjectId);
+    //         }
+    //     }
+    // }
 
 
-    [ServerRpc]
-    void RequestVerifyCollisionServerRpc(ulong otherPlayerId, ServerRpcParams rpcParams = default)
-    {
-        // This function will actually run on the server
-        HandleCollision(otherPlayerId);
-    }
+    // [ServerRpc]
+    // void RequestVerifyCollisionServerRpc(ulong otherPlayerId, ServerRpcParams rpcParams = default)
+    // {
+    //     // This function will actually run on the server
+    //     HandleCollision(otherPlayerId);
+    // }
 
-    private void HandleCollision(ulong otherPlayerId)
-    {
-        if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(otherPlayerId, out NetworkObject otherPlayerNetworkObject))
-        {
-            PlayerMovement otherPlayer = otherPlayerNetworkObject.GetComponent<PlayerMovement>();
-            if (otherPlayer != null)
-            {
-                // Further validation could be done here to make sure the collision is valid
-                DestroyPlayerNetworked(otherPlayer.gameObject);
-            }
-        }
-        else
-        {
-            Debug.LogError("Failed to find the network object for player ID: " + otherPlayerId);
-        }
-    }
+    // private void HandleCollision(ulong otherPlayerId)
+    // {
+    //     if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(otherPlayerId, out NetworkObject otherPlayerNetworkObject))
+    //     {
+    //         PlayerMovement otherPlayer = otherPlayerNetworkObject.GetComponent<PlayerMovement>();
+    //         if (otherPlayer != null)
+    //         {
+    //             // Further validation could be done here to make sure the collision is valid
+    //             DestroyPlayerNetworked(otherPlayer.gameObject);
+    //         }
+    //     }
+    //     else
+    //     {
+    //         Debug.LogError("Failed to find the network object for player ID: " + otherPlayerId);
+    //     }
+    // }
 
-    private void DestroyPlayerNetworked(GameObject player)
-    {
-        // Assuming proper authority checks are in place
-        player.GetComponent<NetworkObject>().Despawn();
-        Destroy(player);
-    }
+    // private void DestroyPlayerNetworked(GameObject player)
+    // {
+    //     // Assuming proper authority checks are in place
+    //     player.GetComponent<NetworkObject>().Despawn();
+    //     Destroy(player);
+    // }
 
 }
